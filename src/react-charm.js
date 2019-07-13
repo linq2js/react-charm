c249b38177933a81a4dcd74c716d912c8c08c2d4
hung.nguyen <hung.nguyen@dfo.global>
2019-07-13 16:07:14 +0700

    implement hoc

--- src/react-charm.js
+++ src/react-charm.js
@@ -448,6 +448,29 @@ export const store = {
   subscribe
 };
 
+export function hoc(...callbacks) {
+  return callbacks.reduce(
+    (nextHoc, callback) => Component => {
+      const MemoComponent = memo(Component);
+
+      return props => {
+        // callback requires props and Comp, it must return React element
+        if (callback.length > 1) {
+          return callback(props, MemoComponent);
+        }
+        let newProps = callback(props);
+        if (newProps === false) return null;
+        if (!newProps) {
+          newProps = props;
+        }
+
+        return createElement(MemoComponent, newProps);
+      };
+    },
+    Component => Component
+  );
+}
+
 function executeEffect(effect, ...args) {
   return effect(
     {
